d498a6b75d1f96233157833bb30885094cc46880
Jacob Andresen <jacob.andresen@gmai.com>
2013-06-01 18:24:19 +0200

    various fixes

--- TagMap.js
+++ TagMap.js
@@ -1,62 +1,87 @@
 //2013 TagMap. ksa.kk.dk <jacob.andresen@gmail.com>
-function TagMap (mapDiv, infoDiv) {
-
-    //begin:config
-    var cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png');
-    var OSM       = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-    var baseMaps = { "cloudmade": cloudmade, "OSM": OSM };
-    function getTagStyle ( tags ) {
-            switch (tags) {
-                case 'test101':
-                    return {
-                        radius: 8,
-                        fillColor : "#ffcc47",
-                        color: "#ff0000",
-                        weight: 5,
-                        opacity: 1,
-                        fillOpacity: 0.8
-                    };
-
-                default:
-                    return {
-                        radius: 8,
-                        fillColor: "#cccccc",
-                        color: "#444",
-                        weight: 1,
-                        opacity: 1,
-                        fillOpacity: 0.8
-                    };
+function TagMap (options) {
+    var conf = options ||{
+        api: "api.php",
+        content: "content_da",
+        mapDiv: 'map',
+        infoDiv: 'info',
+        baseMaps: {
+            "Historisk atlas": new L.TileLayer('http://tile.historiskatlas.dk/54/{z}/{x}/{y}.jpg'),
+            "OSM": new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'),
+            "cloudmade": new L.TileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png')
+        },
+        tagConfig : [
+        {
+            name: "test101",
+            style: {
+                radius: 8,
+                fillColor: "#ffcc47",
+                color: "#ff0000",
+                weight: 5,
+                opacity: 1,
+                fillOpacity: 0.8
             }
+        },
+        {
+            name: "test102",
+            style: {
+                radius: 8,
+                fillColor: "#cccc47",
+                color: "#ffff00",
+                weight: 5,
+                opacity: 0.5,
+                fillOpacity: 0.3
+            }
+        }
+       ],
+       lat: 55.63405,
+       lng: 12.59938,
+       zoom: 14
+    };
+
+    var layers = [];
+    $.each( conf.baseMaps, function (a, b) { layers.push(b); });
+
+    var drawnItems;
+    function getTagStyle ( tagsIn ) {
+          for( var t in conf.tagConfig) {
+              if (tagsIn == conf.tagConfig[t].name) {
+                  return conf.tagConfig[t].style ;
+              }
+          };
     }
 
-    var map = new L.Map(mapDiv, {
-        center: new L.LatLng(55.63405, 12.59938),
-        zoom: 15,
-        layers: [cloudmade, OSM],
-    });
-    L.control.layers(baseMaps).addTo(map);
-    //end:config
+    function createMap () {
+        var map = new L.Map(conf.mapDiv, {
+            center: new L.LatLng(conf.lat, conf.lng),
+            zoom: conf.zoom, //15,
+            layers: layers,
+        });
+        L.control.layers(conf.baseMaps).addTo(map);
+        return map;
+    }
+    var map = createMap();
 
     var exports = {};
-    var proxy = "api.php";
     var layerData;
     var layer;
 
     function load (tags, cb) {
-        $.get( proxy, {
+        $.get( conf.api, {
             type: "mapdata", tags: tags }).done( function (data) {
             cb($.parseJSON(data));
         });
     }
 
-    function info (layerData) { $("#" + infoDiv).html(layerData.content); }
-    function overlay (layerData) { $("#" + infoDiv).html(layerData.id); }
+    function info (layerData) { $("#" + conf.infoDiv).html(layerData[conf.content]); }
+    function overlay (layerData) { $("#" + conf.infoDiv).html(layerData.name); }
 
     exports.show = function (tags) {
         var i;
-         load ( tags, function (data) {
+        exports.clear();
+        load ( tags, function (data) {
             layerData = data;
-             for(i = 0 ; i < layerData.length ; i++) {
+             for(i = 0 ; i < layerData.length-1 ; i++) {
                 try {
                     var geo = $.parseJSON(layerData[i].geometry);
                     var geoJsonLayer;
@@ -68,14 +93,16 @@ function TagMap (mapDiv, infoDiv) {
                             }
                         })
                     } else {
-                    geoJsonLayer = L.geoJson(geo,
-                        { style:   getTagStyle(layerData[i].tags) }
-                      );
+                        geoJsonLayer = L.geoJson(geo,
+                            { style:   getTagStyle(layerData[i].tags) }
+                        );
                     }
                     geoJsonLayer.layerData = layerData[i];
-                    geoJsonLayer.on("click", function (e) { info(this.layerData); });
+                    geoJsonLayer.on("click", function (e) {
+                        info(this.layerData);
+                    });
                     geoJsonLayer.on("mouseover", function (e) { overlay(this.layerData); });
-                   map.addLayer(geoJsonLayer);
+                    map.addLayer(geoJsonLayer);
                 }catch (e){
                     console.log("error:%o", e);
                 }
@@ -84,56 +111,63 @@ function TagMap (mapDiv, infoDiv) {
     };
 
     exports.clear = function () {
-
-        map.eachLayer( function (a, b) {
-            console.log("map:%o", a);
+        map.eachLayer( function (layer) {
+            if (layer.layerData){
+                map.removeLayer(layer);
+            }
         });
-
     };
 
-
     exports.edit = function (tags) {
-        var drawnItems = new L.FeatureGroup();
-        function edit (layerData) {
+        drawnItems = new L.FeatureGroup();
+
+        function editData (layerData) {
             $('#id').val( layerData.id );
             $('#name').val( layerData.name );
-            $('#content').val( layerData.content );
+            $('#content_da').val( layerData.content_da );
+            $('#content_en').val( layerData.content_en );
             $('#geometry').val( layerData.geometry);
             $('#tags').val( layerData.tags);
-        }
-
-        function remove (layer) {
-            console.log("TODO: remove data on the server: %o", layer);
         }
 
         function create (layer, cb) {
            $('#id').val("");
            $('#name').val("");
-           $('#content').val("");
-           $('#tags').val(tags);
-           $.get( proxy, {
-                type: "createmapdata",
-                name: $('#name').val(),
-                content: $('#content').val(),
-                geometry: JSON.stringify(layer.toGeoJSON()),
-                tags: $('#tags').val()
-            }).done( function (data) {
-                cb(data);
-            });
+           $('#content_da').val("");
+
+           if ($('#tags').val() == "")  {
+               alert('missing value for tag');
+               drawnItems.removeLayer(layer);
+           }else {
+               $.get( conf.api, {
+                   type: "createmapdata",
+                   name: $('#name').val(),
+                   content_da: $('#content_da').val(),
+                   geometry: JSON.stringify(layer.toGeoJSON()),
+                   tags: $('#tags').val()
+                }).done( function (data) {
+                    cb(data);
+                });
+            }
          }
 
          function updateInfo (layer) {
-             $.get( proxy, {
+             if ($('#tags').val() == "") {
+                 alert('missing tags');
+             }
+
+             $.get( conf.api, {
                 id : $("#id").val(),
                 type: "createmapdata",
                 name: $('#name').val(),
-                content: $('#content').val(),
+                content_da: $('#content_da').val(),
+                content_en: $('#content_en').val(),
                 geometry: JSON.stringify(layer.toGeoJSON()),
                 tags: $('#tags').val()
             }).done(function (data) {
                 console.log("update success:%o", data);
             });
-         }
+        }
 
         function updateLayer( layer) {
             layer = layer;
@@ -148,38 +182,36 @@ function TagMap (mapDiv, infoDiv) {
         }
 
         load( tags, function (data) {
+            exports.clear();
             layerData = data;
             var i;
-            for(i = 0 ; i < layerData.length ; i++) {
-                if (layerData[i].content !== undefined) {
-                    try {
-                       var geo = $.parseJSON(layerData[i].geometry);
-                       var geoJsonLayer = L.geoJson(geo,
-                         { style:   getTagStyle(layerData[i].tags) }
-                       );
-                       geoJsonLayer.layerData = layerData[i];
-                       geoJsonLayer.on("click", function (e) {
-                            edit(this.layerData);
-                            layer = e.layer;
+            for(i = 0 ; i < layerData.length-1 ; i++) {
+                try {
+                    if (layerData[i].geometry) {
+                        var geo = $.parseJSON(layerData[i].geometry);
+                        var geoJsonLayer  = L.GeoJSON.geometryToLayer(geo);
+                        geoJsonLayer.layerData = layerData[i];
+                        geoJsonLayer.on("click", function (ev) {
+                            layer = geoJsonLayer;
+                            editData(this.layerData);
                         });
-                       map.addLayer(geoJsonLayer);
-                    }catch (e){
-                       // console.log("rendering failed:%o", e);
+                        drawnItems.addLayer(geoJsonLayer);
                     }
+                }catch (e){
+                    console.log("rendering failed:%o", e);
                 }
             }
-
             map.addLayer(drawnItems);
+
             map.on('draw:created', function (e) {
-                var type = e.layerType,
-                    drawnLayer = e.layer;
+                var type = e.layerType, drawnLayer = e.layer;
                 drawnItems.addLayer(drawnLayer);
                 create(drawnLayer,  function cb(data) {
                      drawnLayer.layerData = $.parseJSON(data)[0];
                      layer = drawnLayer;
+                     editData(drawnLayer.layerData);
                      drawnLayer.on("click", function (e) {
-                         edit(this.layerData);
-                         layer = e.layer;
+                         editData(drawnLayer.layerData);
                      });
                 });
             });
@@ -190,9 +222,27 @@ function TagMap (mapDiv, infoDiv) {
                 });
             });
 
-            map.on('draw:deleted', function (e) { });
+         //   map.on('draw:deleted', function (e) { });
          });
-         $('#saveButton').on('click', function () { updateInfo(layer); });
+         $('#saveButton').on('click', function () {
+             console.log("savebutton click:%o", layer);
+             updateInfo(layer);
+         });
+    };
+
+    exports.remove = function () {
+        $.get( conf.api, {
+            id : $("#id").val(),
+            type: "deletemapdata",
+            name: $('#name').val(),
+            content_da: $('#content_da').val(),
+            content_en: $('#content_en').val(),
+            geometry: JSON.stringify(layer.toGeoJSON()),
+            tags: $('#tags').val()
+        }).done(function (data) {
+           console.log("delete success:%o", data);
+           drawnItems.removeLayer(layer);
+        });
     };
     return exports;
 }
