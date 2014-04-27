da328113b957065968024e8ec8f8647e5d2c9efa
Jacob Andresen <jacob.andresen@gmail.com>
2014-04-27 15:25:27 +0200

    requirejs hacking

---
+++ js/edit.js
@@ -0,0 +1,73 @@
+require.config({
+    paths: {
+      "jquery": "libs/jquery/jquery",
+      "leaflet": "libs/Leaflet/leaflet",
+      "leafletdraw": "libs/Leaflet-draw/dist/leaflet.draw",
+      "tagmap": "TagMap"
+    },
+
+    use: {
+        leaflet: {
+            attach: 'L'
+        }
+    }
+});
+
+
+require(['jquery', 'tagmap' ], function ($, TagMap) {
+
+    var tagmap = new TagMap({
+        api           :      "/1/index.php", 
+        content       :      "content_da",
+        mapDiv        :      'map',
+        infoDiv       :      'info',
+        tagSelectorDiv: 'divs',
+        baseMaps      : {
+	     "OSM": new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+        },
+        tagConfig : [{
+	    name: "test102",
+	    style: {
+		radius: 8,
+		fillColor: "#cccc47",
+		color: "#ffff00",
+		weight: 5,
+		opacity: 0.5,
+		fillOpacity: 0.3
+	    }
+       }],
+       lat: 55.63405,
+       lng: 12.59938,
+       zoom: 14
+    });
+            
+    tagmap.createTagSelector();
+	     
+    $('#tags').val("default");
+    tagmap.edit("default");
+    $('#edit').on('click', function () {
+        $('#id').val("");
+        $('#header_en').val("");
+        $('#content_da').val("");
+	$('#content_en').val("");
+	$('#name').val("");
+	$('#tags').val($('#tagsQuery').val());
+	tagmap.edit($('#tagsQuery').val());
+    });
+
+    $('#saveButton').on('click', function () {
+      tagmap.updateInfo();  
+    });
+
+    $('#id').attr("disabled", true);
+    $('#name').attr("disabled", true);
+    $('#header_en').attr("disabled", true);
+    $('#content_da').attr("disabled", true);
+    $('#content_en').attr("disabled", true);
+    $('#remove').attr("disabled", true);
+    $('#save').attr("disabled", true); 
+    $('#remove').on('click', function () { 
+       tagmap.remove();
+    });
+    $('#clear').on('click', function () { tagmap.clear(); });
+})
---
+++ js/show.js
@@ -0,0 +1,52 @@
+require.config({
+    paths: {
+      "jquery": "libs/jquery/jquery",
+      "leaflet": "libs/Leaflet/leaflet",
+      "leafletdraw": "libs/Leaflet-draw/dist/leaflet.draw",
+      "tagmap": "TagMap"
+    },
+
+    use: {
+        leaflet: {
+            attach: 'L'
+        }
+    }
+});
+
+require(['jquery', 'tagmap', 'leaflet', 'leafletdraw'], function ($, TagMap, L) {
+
+    var tagmap = new TagMap({
+        api               : "/1/index.php",
+        language          : "da",
+        mapDiv            : 'map',
+        infoDiv           : 'info',
+        tagSelectorDiv    : 'divs',
+        showLayerControls : true,
+        enableMouseover   : false,
+        baseMaps: {
+            "OSM": new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+        },
+        tagConfig : [
+        {
+           name: "default",
+           style: {
+               radius: 8,
+               fillColor: "#ffcc47",
+               color: "#ff0000",
+               weight: 5,
+               opacity: 1,
+               fillOpacity: 0.8
+          }
+        }
+        ],
+        lat: 55.67899,
+        lng: 12.57522,
+        zoom: 12
+   });
+    
+   tagmap.createTagSelector();
+    
+   $('#display').on('click', function () { tagmap.show( $('#tagsQuery').val() ); });
+   $('#clear').on('click', function () { tagmap.clear(); });
+
+});
