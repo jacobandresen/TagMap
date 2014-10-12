2827c5ad934e886ebb52951b89fd5859a9596a29
Jacob Andresen <jacob.andresen@gmail.com>
2014-10-12 21:22:30 +0200

    do not load javascript when writing text for hashbang indexing

--- public/show.php
+++ public/show.php
@@ -3,7 +3,6 @@
 <head>
     <title>TagMap show</title>
     <meta charset="UTF-8">
-    <meta name="fragment" content="!"/>
 <?php
 require_once("TagMap.php");
 $f = new TagMap("http://localhost:8080/1/index.php");
@@ -11,7 +10,6 @@ print $f->getHeader();
 ?>
 </head>
 <body>
-
     <table width="100%">
         <tr>
             <td>
@@ -23,18 +21,18 @@ print $f->getHeader();
         </tr>
         <tr>
             <td>
-<div id="map" style="height: 500px; border: 1px solid #ccc">
-<?php if ($f->hasFragment()) { print $f->getText(); } ?>
-</div>
-<div id="info" style="width: 500px; height: 500px; border: 1px solid #ccc">
+                <div id="map" style="height: 500px; border: 1px solid #ccc">
+                    <?php if ($f->hasFragment()) { print $f->getText(); } ?>
+                </div>
+                    <div id="info" style="width: 500px; height: 500px; border: 1px solid #ccc">
                </div>
             </td>
         </tr>
     </table>
-
 <?php
-print $f->getJS();
+if (!$f->hasFragment()){
+    print $f->getJS();
+}
 ?>
-
 </body>
 </html>
